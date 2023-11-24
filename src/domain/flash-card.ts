26ce0810f12828b9f338506a8361f2eed50ffcda
Yuri Galvao <yuri.gvao@gmail.com>
2023-11-24 01:13:31 -0300

    create methods to update

--- src/domain/flash-card.ts
+++ src/domain/flash-card.ts
@@ -1,3 +1,4 @@
+import { InvalidParameter } from "../error";
 import Entity from "./entity";
 
 type FlashCardProps = {
@@ -9,12 +10,21 @@ type FlashCardProps = {
 export class FlashCard extends Entity {
 	question: string;
 	answer: string;
-    createdAt: Date;
+	createdAt: Date;
 
 	constructor({ id, question, answer }: FlashCardProps) {
 		super(id);
 		this.question = question;
 		this.answer = answer;
-        this.createdAt = new Date()
+		this.createdAt = new Date();
+	}
+
+	updateQuestion(newQuestion: string) {
+		if (!newQuestion || newQuestion!.length < 3) new InvalidParameter();
+		this.question = newQuestion;
+	}
+	updateAnswer(newAnswer: string) {
+		if (!newAnswer || newAnswer.length < 3) new InvalidParameter();
+		this.answer = newAnswer;
 	}
 }
--- src/test/flash-card.test.ts
+++ src/test/flash-card.test.ts
@@ -1,39 +1,78 @@
-import { beforeEach, expect, test } from "vitest";
+import { assert, beforeEach, describe, expect, test } from "vitest";
 import { FlashCard } from "../domain/flash-card";
+import { InvalidParameter } from "../error";
 
 let sut: FlashCard;
 
-beforeEach(() => {
-	sut = new FlashCard({ question: "qual meu nome?", answer: "Yuri" });
-});
-
-test("should instance a flash card", () => {
-	expect(sut).toBeInstanceOf(FlashCard);
-	expect(sut.id).toBeDefined();
-	expect(sut.question).toBe("qual meu nome?");
-	expect(sut.answer).toBe("Yuri");
-});
-
-test("must ensure the id you choose", () => {
-	const sut = new FlashCard({
-		id: "any_id",
-		answer: "any_answer",
-		question: "any_question",
+describe("flashcard", () => {
+	beforeEach(() => {
+		sut = new FlashCard({ question: "qual meu nome?", answer: "Yuri" });
 	});
 
-	expect(sut.id).toBe("any_id");
-});
-
-test("should update flashcard with update method", () => {
-	const sut = new FlashCard({
-		id: "any_id",
-		answer: "any_answer",
-		question: "any_question",
+	test("should instance a flash card", () => {
+		expect(sut).toBeInstanceOf(FlashCard);
+		expect(sut.id).toBeDefined();
+		expect(sut.question).toBe("qual meu nome?");
+		expect(sut.answer).toBe("Yuri");
 	});
 
-	sut.updateQuestion("question_updated");
-	sut.updateAnswer("answer_updated");
+	test("must ensure the id you choose", () => {
+		const sut = new FlashCard({
+			id: "any_id",
+			answer: "any_answer",
+			question: "any_question",
+		});
 
-	expect(sut.question).toBe("question_updated");
-	expect(sut.answer).toBe("answer_updated");
+		expect(sut.id).toBe("any_id");
+	});
+
+	describe("#updateQuestion", () => {
+		test("should update flashcard with update method", () => {
+			const sut = new FlashCard({
+				id: "any_id",
+				answer: "any_answer",
+				question: "any_question",
+			});
+
+			sut.updateQuestion("question_updated");
+
+			expect(sut.question).toBe("question_updated");
+		});
+
+		test("should`n update flashcard with invalid parameters", () => {
+			const sut = new FlashCard({
+				id: "any_id",
+				answer: "any_answer",
+				question: "any_question",
+			});
+
+			expect(() => sut.updateQuestion("")).toThrowError(`invalid parameter`);
+		});
+	});
+
+	describe("#updateAnswer", () => {
+		test("should update flashcard with update method", () => {
+			const sut = new FlashCard({
+				id: "any_id",
+				answer: "any_answer",
+				question: "any_question",
+			});
+
+			sut.updateAnswer("answer_updated");
+
+			expect(sut.answer).toBe("answer_updated");
+		});
+
+		test("should`n update flashcard with invalid parameters", () => {
+			const sut = new FlashCard({
+				id: "any_id",
+				answer: "any_answer",
+				question: "any_question",
+			});
+
+			expect(() => {
+				sut.updateAnswer("");
+			}).toThrowError("invalid parameters");
+		});
+	});
 });
