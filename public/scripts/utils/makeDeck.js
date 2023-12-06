7395e720276190142ac2926922e448920f745c66
Yuri Galvao <yuri.gvao@gmail.com>
2023-12-06 18:40:28 -0300

    upload makeDeck

--- public/scripts/utils/makeDeck.js
+++ public/scripts/utils/makeDeck.js
@@ -1,5 +1,23 @@
 import flashCardsApi from "./flashCardsApi.js";
 import { Observer } from "./observer.js";
+import Card from '../entity/card.js'
+
+/**
+ * @typedef {Object} Repository
+ * @property {Function} set - Função para definir o estado do repositório.
+ */
+
+/**
+ * @typedef {Object} Deck
+ * @property {() => Card[]} getState - Obtém o estado atual do deck.
+ * @property {(card: { question: any, answer: any }) => Promise<void>} createCard - Cria um novo card.
+ * @property {(listener: any) => void} subscribe - Adiciona um observador.
+ * @property {(id: string) => () => Promise<void>} deleteById - Exclui um card por ID.
+ * @property {(id: any, newValue: any) => Promise<void>} updateById - Atualiza um card por ID.
+ * 
+ * @param {Repository} repository - O repositório utilizado.
+ * @returns {Deck} - Instância do deck.
+ */
 
 export default function MakeDeck(repository) {
 	const observer = Observer();
@@ -13,20 +31,40 @@ export default function MakeDeck(repository) {
 		setState(flashCards);
 	})();
 
+	/**
+	 * Obtém o estado atual do deck.
+	 * @returns {Card[]} - Array de cards.
+	 */
 	const getState = () => cards;
 
+	/**
+	 * Define o estado do deck.
+	 * @param {Card[]} newState - Novo estado do deck.
+	 */
 	const setState = (newState) => {
 		cards = newState;
 		repository.set(newState);
 		observer.emit();
 	};
 
+	/**
+	 * Cria um novo card.
+	 * @param {Object} param0 - Parâmetros para criação do card.
+	 * @param {string} param0.question - Pergunta do card.
+	 * @param {string} param0.answer - Resposta do card.
+	 * @returns {Promise<void>} - Promise indicando a conclusão da criação.
+	 */
 	async function createCard({ question, answer }) {
 		const newCard = await api.create({ question, answer });
 		console.log(`created new card: `, newCard);
 		setState([...cards, newCard]);
 	}
 
+	/**
+	 * Exclui um card por ID.
+	 * @param {string} id - ID do card a ser excluído.
+	 * @returns {Promise<void>} - Promise indicando a conclusão da exclusão.
+	 */
 	const deleteById = (id) => async () => {
 		const { idDeleted, message } = await api.deleteById(id);
 		console.log(`Delete id: ${idDeleted}\n`, message);
@@ -35,6 +73,12 @@ export default function MakeDeck(repository) {
 		alert(message);
 	};
 
+	/**
+	 * Atualiza um card por ID.
+	 * @param {any} id - ID do card a ser atualizado.
+	 * @param {any} newValue - Novos valores para o card.
+	 * @returns {Promise<void>} - Promise indicando a conclusão da atualização.
+	 */
 	const updateById = async (id, newValue) => {
 		await api.updateById(id, newValue);
 		console.log(`updateById: `, newValue);
