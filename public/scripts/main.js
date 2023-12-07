e0bfec08d1f919cd8f922b246c15be49214f74f5
Yuri Galvao <yuri.gvao@gmail.com>
2023-12-06 21:19:26 -0300

    remove render module

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,116 +1,43 @@
 import CardService from './services/CardService.js';
 import LocalStorageRepository from "./repository/LocalStorageRepository.js";
+import RenderCards from './utils/renderCards.js'
 
 const formQuestion = document.getElementById('form-flashcards')
 
-const repository = LocalStorageRepository();
+const repository = new LocalStorageRepository();
 const cardService = new CardService(repository)
+const renderCards = new RenderCards(cardService)
 
-formQuestion.addEventListener('submit', event => {
+/**
+ * 
+ * @param {SubmitEvent} event 
+ */
+function handlerEvent(event) {
     event.preventDefault()
     const inputs = Array.from(formQuestion.querySelectorAll('input'))
     const [question, answer] = inputs.map(input => input.value)
 
     cardService.addNewCard({ question, answer })
-})
+}
 
-cardService.onChangeCards(() => {
-    renderCards(cardService.cards)
-})
-renderCards(cardService.cards)
+const logCardsLength = (cards = cardService.cards) => console.log(`${cards.length} para ${cardService.cards.length} cartas`)
+// logCardsLength()
 
-
-function renderCards(cards) {
-    const deck = document.getElementById('deck')
-    deck.innerHTML = ''
-
-    const elements = cards
-        .map(card => {
-            const intl = new Intl.DateTimeFormat('pt-BR')
-            const li = createElement('li', { textContent: `${card.isReview ? 'review' : 'not review'}: ${intl.format(card.reviewAt)}` })
-            
-            const question = createElement('h3', { textContent: card.question })
-            const answerElement = createElement('p', { textContent: card.answer })
-            const buttonDelete = createButtonElement('delete', { onclick: () => { cardService.deleteCardById(card.id) } })
-            const choiceElement = createChoicesElement(
-                () => { cardService.rightCard(card) },
-                () => { cardService.leftCard(card) },
-            )
-
-            li.classList.add('deck__card')
-            question.classList.add('cards__question')
-            answerElement.classList.add('cards__answer')
-            answerElement.dataset.hide = true
-            choiceElement.dataset.hide = true
-
-            const formQuestion = createFormAnswerElement(event => {
-                const input = event.target.querySelector('input')
-                const button = event.target.querySelector('button')
-                answerElement.dataset.hide = false
-                choiceElement.dataset.hide = false
-                button.dataset.hide = true
-            })
-
-            li.append(question, buttonDelete, answerElement, formQuestion, choiceElement)
-
-            return li
+if (cardService.cards.length < 5) {
+    const quant = 10 - cardService.cards.length
+    console.log(`inserindo ${quant}`)
+    for (let i = 0; i < quant; i++) {
+        const random = Math.random() * 10000
+        const idTemp = Math.round(random)
+        console.log(`inserido o id: ${idTemp}`);
+        cardService.addNewCard({
+            answer: `resposta - ${idTemp}`,
+            question: `pergunta - ${idTemp}`,
         })
-    deck.append(...elements)
-}
-
-/**
- * @param {string} tagName 
- * @param {HTMLElement} options 
- * @return {HTMLElement}
- */
-function createElement(tagName, { textContent, ...options } = {}) {
-    const element = document.createElement(tagName)
-    element.textContent = textContent
-    for (const key in options) {
-        element.setAttribute(key, options[key])
     }
-    return element
 }
 
-/**
- * @param {string} textContent 
- * @param {HTMLButtonElement} options 
- * @return {HTMLButtonElement}
- */
-function createButtonElement(textContent, { onclick = () => { }, ...options } = {}) {
-    const button = createElement('button', { textContent, type: 'button', ...options })
-    button.onclick = onclick
-    return button
-}
-
-/**
- * 
- * @param {((this: GlobalEventHandlers, ev: SubmitEvent) => any) | null} onSubmit 
- * @returns 
- */
-function createFormAnswerElement(onSubmit) {
-    const form = createElement('form')
-    form.classList.add('form-question')
-    form.onsubmit = event => {
-        event.preventDefault()
-        onSubmit(event)
-    }
-    const htmlString = `
-            <input type="text" name="answer__input" id="answer__input" autofocus placeholder="Sua resposta aqui">
-            <button type="submit">Responder</button>
-        `
-    form.innerHTML = htmlString
-
-    return form
-}
-
-/**
- * @returns {HTMLDivElement}
- */
-function createChoicesElement(right = () => { }, left = () => { }) {
-    const div = createElement('div')
-    const buttonAcertei = createButtonElement('acertei', { onclick: right })
-    const buttonErrei = createButtonElement('Errei', { onclick: left })
-    div.append(buttonAcertei, buttonErrei)
-    return div
-}
+formQuestion.addEventListener('submit', handlerEvent)
+cardService.onChangeCards(() => renderCards.render(cardService.cards))
+cardService.onChangeCards(logCardsLength)
+renderCards.render(cardService.cards)
