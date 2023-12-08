79a9e979df0ee53755d022561b15e00347896737
Yuri Galvao <yuri.gvao@gmail.com>
2023-12-08 11:11:03 -0300

    update main

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,7 +9,6 @@ const cardService = new CardService(repository)
 const renderCards = new RenderCards(cardService)
 
 /**
- * 
  * @param {SubmitEvent} event 
  */
 function handlerEvent(event) {
@@ -20,24 +19,11 @@ function handlerEvent(event) {
     cardService.addNewCard({ question, answer })
 }
 
-const logCardsLength = (cards = cardService.cards) => console.log(`${cards.length} para ${cardService.cards.length} cartas`)
-// logCardsLength()
-
-if (cardService.cards.length < 5) {
-    const quant = 10 - cardService.cards.length
-    console.log(`inserindo ${quant}`)
-    for (let i = 0; i < quant; i++) {
-        const random = Math.random() * 10000
-        const idTemp = Math.round(random)
-        console.log(`inserido o id: ${idTemp}`);
-        cardService.addNewCard({
-            answer: `resposta - ${idTemp}`,
-            question: `pergunta - ${idTemp}`,
-        })
-    }
-}
-
 formQuestion.addEventListener('submit', handlerEvent)
-cardService.onChangeCards(() => renderCards.render(cardService.cards))
-cardService.onChangeCards(logCardsLength)
-renderCards.render(cardService.cards)
+repository.subscribe(() => {
+    const cards = cardService.getCardsReviewIs(true)
+    renderCards.render(cards)
+})
+
+const cards = cardService.getCardsReviewIs(true)
+renderCards.render(cards)
